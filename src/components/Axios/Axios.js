7e9794ebe89ab8ed557caf6ed422b2057f37b1cf
martiros888 <harutunyan.martiros@mail.ru>
2021-01-28 22:50:46 +0400

    added procfile

--- src/components/Axios/Axios.js
+++ src/components/Axios/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const post =async (url,data) =>{
-    const response = await axios.post(`http://localhost:5000${url}`,data)
+    const response = await axios.post(`https://git.heroku.com/wine-production.git${url}`,data)
     return response.data
 }
