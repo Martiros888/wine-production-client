cb9f1a6e945f596358a6294cee222a53331ba9be
martiros888 <harutunyan.martiros@mail.ru>
2021-01-29 10:23:37 +0400

    changed the url

--- src/components/Axios/Axios.js
+++ src/components/Axios/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const post =async (url,data) =>{
-    const response = await axios.post(`https://git.heroku.com/wine-production.git${url}`,data)
+    const response = await axios.post(`https://wine-production-server.herokuapp.com${url}`,data)
     return response.data
 }
